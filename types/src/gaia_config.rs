47ea081b05f08e23e66927cac3ccdea94ef7ba17
bekauz <bekauz@protonmail.com>
2025-06-03 16:58:18 +0200

    extend gaia config with vault denom

--- types/src/gaia_config.rs
+++ types/src/gaia_config.rs
@@ -6,5 +6,9 @@ pub struct GaiaStrategyConfig {
     pub grpc_port: String,
     pub chain_id: String,
     pub mnemonic: String,
-    pub denom: String,
+    // native chain denom
+    pub chain_denom: String,
+
+    // deposit denom routed from ethereum via IBC-Eureka
+    pub btc_denom: String,
 }
