cfd5f6ecaef5223ab31b986d99badeaa309713c2
bekauz <bekauz@protonmail.com>
2025-06-08 17:23:59 +0300

    read domain cfg paths from env

--- strategist/src/bin/runner.rs
+++ strategist/src/bin/runner.rs
@@ -1,22 +1,27 @@
-use std::error::Error;
 use dotenv::dotenv;
+use std::{env, error::Error, fs};
 use valence_strategist_utils::worker::ValenceWorker;
 
 use strategist::strategy_config::Strategy;
 
-// default configuration paths, should be overridden by environment variables
-const NEUTRON_CFG_PATH: &str = "config/neutron_config.toml";
-const ETHEREUM_CFG_PATH: &str = "config/ethereum_config.toml";
-const GAIA_CFG_PATH: &str = "config/gaia_config.toml";
-
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Load environment variables
+    // load environment variables
     dotenv().ok();
 
+    // get configuration paths from environment variables
+    let neutron_cfg_path = env::var("NEUTRON_CFG_PATH")?;
+    let ethereum_cfg_path = env::var("ETHEREUM_CFG_PATH")?;
+    let gaia_cfg_path = env::var("GAIA_CFG_PATH")?;
+
+    println!("Using configuration files:");
+    println!("  Neutron: {}", neutron_cfg_path);
+    println!("  Ethereum: {}", ethereum_cfg_path);
+    println!("  Gaia: {}", gaia_cfg_path);
+
     // initialize the strategy from configuration files
-    let strategy = Strategy::from_files(NEUTRON_CFG_PATH, GAIA_CFG_PATH, ETHEREUM_CFG_PATH).await?;
+    let strategy =
+        Strategy::from_files(&neutron_cfg_path, &gaia_cfg_path, &ethereum_cfg_path).await?;
 
     let _strategist_join_handle = strategy.start();
 
