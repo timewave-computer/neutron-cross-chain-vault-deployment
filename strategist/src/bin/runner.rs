6544ecb5ca4cd43522a70ff79c2b44ba91ccc307
bekauz <bekauz@protonmail.com>
2025-06-09 13:18:11 +0300

    move co-processor config to a dedicated type

--- strategist/src/bin/runner.rs
+++ strategist/src/bin/runner.rs
@@ -13,15 +13,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let neutron_cfg_path = env::var("NEUTRON_CFG_PATH")?;
     let ethereum_cfg_path = env::var("ETHEREUM_CFG_PATH")?;
     let gaia_cfg_path = env::var("GAIA_CFG_PATH")?;
+    let coprocessor_cfg_path = env::var("COPROCESSOR_CFG_PATH")?;
 
     println!("Using configuration files:");
     println!("  Neutron: {}", neutron_cfg_path);
     println!("  Ethereum: {}", ethereum_cfg_path);
     println!("  Gaia: {}", gaia_cfg_path);
+    println!("  Co-processor: {}", coprocessor_cfg_path);
 
     // initialize the strategy from configuration files
-    let strategy =
-        Strategy::from_files(&neutron_cfg_path, &gaia_cfg_path, &ethereum_cfg_path).await?;
+    let strategy = Strategy::from_files(
+        &neutron_cfg_path,
+        &gaia_cfg_path,
+        &ethereum_cfg_path,
+        &coprocessor_cfg_path,
+    )
+    .await?;
 
     // start the strategy and get the thread join handle
     let _strategist_join_handle = strategy.start();
