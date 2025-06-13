8c6d1017e25c9c9eacb2a207552cbebc8439b9bf
keyleu <kleuyckx@gmail.com>
2025-06-13 10:33:38 +0100

    change Uint128 to u128

--- deploy/src/bin/ethereum_deploy.rs
+++ deploy/src/bin/ethereum_deploy.rs
@@ -6,7 +6,6 @@ use alloy::{
     sol,
     sol_types::SolValue,
 };
-use cosmwasm_std::Uint128;
 use serde::Deserialize;
 use sp1_sdk::{HashableKey, SP1VerifyingKey};
 use types::{
@@ -50,7 +49,7 @@ struct Vault {
     platform_fee_account: Address,
     strategist_fee_account: Address,
     strategist_fee_ratio_bps: u32,
-    scaling_factor: Uint128,
+    scaling_factor: u128,
     deposit_cap: U256,
     deposit_fee_bps: u32,
     withdraw_rate_bps: u32,
@@ -315,7 +314,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         ibc_transfer_threshold_amt: U256::from(
             parameters.eureka_transfer.ibc_transfer_threshold_amt,
         ),
-        rate_scaling_factor: parameters.vault.scaling_factor,
+        rate_scaling_factor: parameters.vault.scaling_factor.into(),
         rpc_url: parameters.general.rpc_url,
         authorizations: authorization,
         processor: processor_address,
