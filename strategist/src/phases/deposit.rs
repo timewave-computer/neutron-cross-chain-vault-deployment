44a232b22a02b2e5c1cef11ab1a53ec0cf013f03
bekauz <bekauz@protonmail.com>
2025-06-17 13:28:57 +0200

    more deposit phase cleanups; increasing the eth eureka transfer polling duration to 25min

--- strategist/src/phases/deposit.rs
+++ strategist/src/phases/deposit.rs
@@ -22,147 +22,6 @@ use crate::strategy_config::Strategy;
 const DEPOSIT_PHASE: &str = "deposit";
 
 impl Strategy {
-    async fn eth_to_gaia_routing(
-        &mut self,
-        eth_rp: &CustomProvider,
-        eth_deposit_acc_bal: U256,
-    ) -> Result<(), Box<dyn Error + Send + Sync>> {
-        let eth_auth_contract = Authorization::new(self.cfg.ethereum.authorizations, &eth_rp);
-
-        // 3. fetch the IBC-Eureka route from eureka client
-        let skip_api_response = match self
-            .ibc_eureka_client
-            .query_skip_eureka_route(eth_deposit_acc_bal.to_string())
-            .await
-        {
-            Ok(r) => r,
-            Err(e) => {
-                warn!(target: DEPOSIT_PHASE, "skip route error: {e}");
-                return Ok(());
-            }
-        };
-
-        // format the response in format expected by the coprocessor and post it
-        // there for proof
-        let coprocessor_input = json!({"skip_response": skip_api_response});
-        info!(target: DEPOSIT_PHASE, "posting skip-api response to co-processor app id: {}", &self.cfg.coprocessor.eureka_circuit_id);
-        let skip_response_zkp = self
-            .coprocessor_client
-            .prove(&self.cfg.coprocessor.eureka_circuit_id, &coprocessor_input)
-            .await?;
-
-        info!(target: DEPOSIT_PHASE, "co_processor zkp post response: {:?}", skip_response_zkp);
-
-        // extract the program and domain parameters by decoding the zkp
-        let (proof_program, inputs_program) = skip_response_zkp.program.decode()?;
-        let (proof_domain, inputs_domain) = skip_response_zkp.domain.decode()?;
-
-        // build the eureka transfer zk message from decoded params
-        let auth_eureka_transfer_zk_msg = eth_auth_contract.executeZKMessage(
-            Bytes::from(inputs_program),
-            Bytes::from(proof_program),
-            Bytes::from(inputs_domain),
-            Bytes::from(proof_domain),
-        );
-
-        // sign and execute the tx & await its tx receipt before proceeding
-        info!(target: DEPOSIT_PHASE, "posting skip-api zkp ethereum authorizations");
-        let zk_auth_exec_response = self
-            .eth_client
-            .sign_and_send(auth_eureka_transfer_zk_msg.into_transaction_request())
-            .await?;
-        eth_rp
-            .get_transaction_receipt(zk_auth_exec_response.transaction_hash)
-            .await?;
-
-        // 4. block execution until the funds arrive to the Cosmos Hub ICA owned
-        // by the Valence Interchain Account on Neutron
-        // TODO: doublecheck the precision conversion here
-        let gaia_ica_balance = Uint128::from_str(&eth_deposit_acc_bal.to_string())?;
-        info!(target: DEPOSIT_PHASE, "gaia ica expected deposit token bal = {gaia_ica_balance}; starting to poll");
-
-        self.gaia_client
-            .poll_until_expected_balance(
-                &self.cfg.gaia.ica_address,
-                &self.cfg.gaia.deposit_denom,
-                gaia_ica_balance.u128(),
-                5,
-                30,
-            )
-            .await?;
-        Ok(())
-    }
-
-    async fn gaia_to_neutron_routing(
-        &mut self,
-        gaia_ica_bal: u128,
-    ) -> Result<(), Box<dyn Error + Send + Sync>> {
-        // TODO: this should look like this but there's some serde issue so using manual
-        // json below for now
-
-        // let ica_ibc_transfer_update_msg: valence_library_utils::msg::ExecuteMsg<
-        //     valence_ica_ibc_transfer::msg::FunctionMsgs,
-        //     valence_ica_ibc_transfer::msg::LibraryConfigUpdate,
-        // > = valence_library_utils::msg::ExecuteMsg::UpdateConfig {
-        //     new_config: valence_ica_ibc_transfer::msg::LibraryConfigUpdate {
-        //         input_addr: None,
-        //         amount: Some(gaia_ica_bal.into()),
-        //         denom: None,
-        //         receiver: None,
-        //         memo: None,
-        //         remote_chain_info: None,
-        //         denom_to_pfm_map: None,
-        //         eureka_config: OptionUpdate::Set(None),
-        //     },
-        // };
-        let ica_ibc_transfer_update_msg = json!({
-            "update_config": {
-                "new_config": {
-                    "amount": gaia_ica_bal.to_string(),
-                    "eureka_config": "none"
-                }
-            }
-        });
-        let ica_ibc_transfer_update_msg = to_json_binary(&ica_ibc_transfer_update_msg)?;
-        info!("update b64: {}", ica_ibc_transfer_update_msg);
-
-        let ica_ibc_transfer_exec_msg: valence_library_utils::msg::ExecuteMsg<
-            valence_ica_ibc_transfer::msg::FunctionMsgs,
-            valence_ica_ibc_transfer::msg::LibraryConfigUpdate,
-        > = valence_library_utils::msg::ExecuteMsg::ProcessFunction(
-            valence_ica_ibc_transfer::msg::FunctionMsgs::Transfer {},
-        );
-
-        info!(target: DEPOSIT_PHASE, "performing ica_ibc_transfer library update & transfer");
-        self.enqueue_neutron(
-            ICA_TRANSFER_LABEL,
-            vec![
-                ica_ibc_transfer_update_msg,
-                to_json_binary(&ica_ibc_transfer_exec_msg)?,
-            ],
-        )
-        .await?;
-
-        info!(target: DEPOSIT_PHASE, "tick: update & transfer");
-        self.tick_neutron().await?;
-
-        info!(target: DEPOSIT_PHASE, "polling for neutron deposit account to receive the funds");
-
-        // 6. block execution until funds arrive to the Neutron program deposit
-        // account
-        self.neutron_client
-            .poll_until_expected_balance(
-                &self.cfg.neutron.accounts.deposit,
-                &self.cfg.neutron.denoms.deposit_token,
-                gaia_ica_bal,
-                5,
-                10,
-            )
-            .await?;
-
-        Ok(())
-    }
-
     /// carries out the steps needed to bring the new deposits from Ethereum to
     /// Neutron (via Cosmos Hub) before depositing them into Mars protocol.
     /// consists of three stages:
@@ -288,6 +147,152 @@ impl Strategy {
                 }
             }
         }
+
+        Ok(())
+    }
+
+    /// carries out the steps needed to route the deposits from Ethereum program deposit
+    /// account to the configured Cosmos Hub ICA managed by Neutron Valence-ICA.
+    async fn eth_to_gaia_routing(
+        &mut self,
+        eth_rp: &CustomProvider,
+        eth_deposit_acc_bal: U256,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        let eth_auth_contract = Authorization::new(self.cfg.ethereum.authorizations, &eth_rp);
+
+        // fetch the IBC-Eureka route from eureka client
+        let skip_api_response = match self
+            .ibc_eureka_client
+            .query_skip_eureka_route(eth_deposit_acc_bal.to_string())
+            .await
+        {
+            // TODO: consider more thorough checks on this response as even in case of an error this
+            // may return something seemingly legit (json-value wise)
+            Ok(r) => r,
+            Err(e) => {
+                warn!(target: DEPOSIT_PHASE, "skip route error: {e}");
+                return Ok(());
+            }
+        };
+
+        // format the response in format expected by the coprocessor and post it
+        // there for proof
+        let coprocessor_input = json!({"skip_response": skip_api_response});
+        info!(target: DEPOSIT_PHASE, "posting skip-api response to co-processor app id: {}", &self.cfg.coprocessor.eureka_circuit_id);
+        let skip_response_zkp = self
+            .coprocessor_client
+            .prove(&self.cfg.coprocessor.eureka_circuit_id, &coprocessor_input)
+            .await?;
+
+        info!(target: DEPOSIT_PHASE, "co_processor zkp post response: {:?}", skip_response_zkp);
+
+        // extract the program and domain parameters by decoding the zkp
+        let (proof_program, inputs_program) = skip_response_zkp.program.decode()?;
+        let (proof_domain, inputs_domain) = skip_response_zkp.domain.decode()?;
+
+        // build the eureka transfer zk message from decoded params
+        let auth_eureka_transfer_zk_msg = eth_auth_contract.executeZKMessage(
+            Bytes::from(inputs_program),
+            Bytes::from(proof_program),
+            Bytes::from(inputs_domain),
+            Bytes::from(proof_domain),
+        );
+
+        // sign and execute the tx & await its tx receipt before proceeding
+        info!(target: DEPOSIT_PHASE, "posting skip-api zkp ethereum authorizations");
+        let zk_auth_exec_response = self
+            .eth_client
+            .sign_and_send(auth_eureka_transfer_zk_msg.into_transaction_request())
+            .await?;
+        eth_rp
+            .get_transaction_receipt(zk_auth_exec_response.transaction_hash)
+            .await?;
+
+        // block execution until the funds arrive to the Cosmos Hub ICA owned
+        // by the Valence Interchain Account on Neutron.
+        // we poll
+        // TODO: doublecheck the precision conversion here
+        let gaia_ica_balance = Uint128::from_str(&eth_deposit_acc_bal.to_string())?;
+        info!(target: DEPOSIT_PHASE, "gaia ica expected deposit token bal = {gaia_ica_balance}; starting to poll");
+
+        // poll for 15sec * 100 = 1500sec = 25min which should suffice for
+        // IBC Eureka routing time of 15min
+        self.gaia_client
+            .poll_until_expected_balance(
+                &self.cfg.gaia.ica_address,
+                &self.cfg.gaia.deposit_denom,
+                gaia_ica_balance.u128(),
+                15,  // every 15 sec
+                100, // for 100 times
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn gaia_to_neutron_routing(
+        &mut self,
+        gaia_ica_bal: u128,
+    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+        // TODO: this should look like this but there's some serde issue so using manual
+        // json below for now
+
+        // let ica_ibc_transfer_update_msg: valence_library_utils::msg::ExecuteMsg<
+        //     valence_ica_ibc_transfer::msg::FunctionMsgs,
+        //     valence_ica_ibc_transfer::msg::LibraryConfigUpdate,
+        // > = valence_library_utils::msg::ExecuteMsg::UpdateConfig {
+        //     new_config: valence_ica_ibc_transfer::msg::LibraryConfigUpdate {
+        //         input_addr: None,
+        //         amount: Some(gaia_ica_bal.into()),
+        //         denom: None,
+        //         receiver: None,
+        //         memo: None,
+        //         remote_chain_info: None,
+        //         denom_to_pfm_map: None,
+        //         eureka_config: OptionUpdate::Set(None),
+        //     },
+        // };
+        let ica_ibc_transfer_update_msg = json!({
+            "update_config": {
+                "new_config": {
+                    "amount": gaia_ica_bal.to_string(),
+                    "eureka_config": "none"
+                }
+            }
+        });
+
+        let ica_ibc_transfer_exec_msg: valence_library_utils::msg::ExecuteMsg<
+            valence_ica_ibc_transfer::msg::FunctionMsgs,
+            valence_ica_ibc_transfer::msg::LibraryConfigUpdate,
+        > = valence_library_utils::msg::ExecuteMsg::ProcessFunction(
+            valence_ica_ibc_transfer::msg::FunctionMsgs::Transfer {},
+        );
+
+        info!(target: DEPOSIT_PHASE, "enqueuing ica_ibc_transfer library update & transfer");
+        self.enqueue_neutron(
+            ICA_TRANSFER_LABEL,
+            vec![
+                to_json_binary(&ica_ibc_transfer_update_msg)?,
+                to_json_binary(&ica_ibc_transfer_exec_msg)?,
+            ],
+        )
+        .await?;
+
+        info!(target: DEPOSIT_PHASE, "tick: update & transfer");
+        self.tick_neutron().await?;
+
+        info!(target: DEPOSIT_PHASE, "polling for neutron deposit account to receive the funds");
+
+        // block execution until funds arrive to the Neutron program deposit
+        // account
+        self.neutron_client
+            .poll_until_expected_balance(
+                &self.cfg.neutron.accounts.deposit,
+                &self.cfg.neutron.denoms.deposit_token,
+                gaia_ica_bal,
+                5,
+                30,
+            )
+            .await?;
 
         Ok(())
     }
