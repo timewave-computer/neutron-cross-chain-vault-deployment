08cb2a62ad8996a0002555a9da9f2e0f4fb259f5
keyleu <kleuyckx@gmail.com>
2025-06-23 17:02:46 +0100

    wbtc vault deployment and initialization scripts

---
+++ strategies/wbtc/deploy/README.md
@@ -0,0 +1,17 @@
+# Deploy instructions
+
+1. If the wasm blobs are not uploaded, run the `neutron_upload.rs` script which will upload all contracts in /packages/src/contracts/cw and output the code ids in `neutron_code_ids.toml`. These file will be used to instantiate the file.
+
+2. Fill in all the information in `neutron.toml` except the coprocessor app fields at the end and run the `neutron_deploy.rs` script which will instantiate all the contracts, trigger the ICA creation and output all relevant addresses in `neutron_strategy_config.toml` and `gaia_strategy_config.toml` which will be used by the strategist.
+
+3. Now that we have deployed on Neutron and we have all relevant addresses in `neutron_strategy_config.toml` and `gaia_strategy_config.toml` we can deploy on Ethereum. For that, input the generated ICA in `ethereum.toml` and run the `ethereum_deploy.rs` script.
+
+4. Now that we have deployed on both Ethereum and Neutron we can finalize the coprocessor apps and get their relevant IDs.
+
+5. Run `neutron_initialization.rs` which will create all the authorizations including the ZK authorization.
+
+6. Run `ethereum_initialization.rs` which will create the relevant IBC Eureka ZK Authorization on the authorization contract that the strategist can execute.
+
+### Notes for Strategist
+
+This vault involves updating all the supervaults LPers configuration for the 2nd phase (maxBTC migration). Therefore, once the migration is completed, the corresponding LP denoms of the supervaults need to be updated because they will be different.
---
+++ strategies/wbtc/types/src/neutron_config.rs
@@ -0,0 +1,116 @@
+use cosmwasm_std::Uint128;
+use serde::{Deserialize, Serialize};
+use valence_strategist_utils::worker::ValenceWorkerTomlSerde;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NeutronStrategyConfig {
+    /// grpc node url
+    pub grpc_url: String,
+    /// grpc node port
+    pub grpc_port: String,
+    /// neutron chain id
+    pub chain_id: String,
+    /// total amount of untrn required to initiate an ibc transfer from neutron
+    pub min_ibc_fee: Uint128,
+
+    /// Mars protocol wbtc contract
+    pub mars_pool: String,
+    /// Supervaults vault addresses
+    pub fbtc_supervault: String,
+    pub lbtc_supervault: String,
+    pub solvbtc_supervault: String,
+    pub ebtc_supervault: String,
+    pub pumpbtc_supervault: String,
+    pub bedrock_supervault: String,
+    pub maxbtc_supervault: String,
+    /// authorizations module
+    pub authorizations: String,
+    /// processor coupled with the authorizations
+    pub processor: String,
+
+    /// all denoms relevant to the neutron-side of strategy
+    pub denoms: NeutronDenoms,
+    /// all accounts relevant to the neutron-side of strategy
+    pub accounts: NeutronAccounts,
+    /// all libraries relevant to the neutron-side of strategy
+    pub libraries: NeutronLibraries,
+    /// All IDs of the coprocessor apps
+    pub coprocessor_app_ids: NeutronCoprocessorAppIds,
+}
+
+impl ValenceWorkerTomlSerde for NeutronStrategyConfig {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NeutronDenoms {
+    /// e.g. WBTC
+    pub deposit_token: String,
+    /// gas fee denom
+    pub ntrn: String,
+    /// supervaults LP share denoms (These need to be updated post migration)
+    pub fbtc_supervault_lp: String,
+    pub lbtc_supervault_lp: String,
+    pub solvbtc_supervault_lp: String,
+    pub ebtc_supervault_lp: String,
+    pub pumpbtc_supervault_lp: String,
+    pub bedrock_supervault_lp: String,
+    pub maxbtc_supervault_lp: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NeutronAccounts {
+    /// Valence ICA
+    pub ica: String,
+    /// deposit account where funds will arrive from cosmos hub
+    pub deposit: String,
+    /// input account from which funds will be deposited into Mars
+    pub mars_deposit: String,
+    /// input accounts for all supervaults
+    pub fbtc_supervault_deposit: String,
+    pub lbtc_supervault_deposit: String,
+    pub solvbtc_supervault_deposit: String,
+    pub ebtc_supervault_deposit: String,
+    pub pumpbtc_supervault_deposit: String,
+    pub bedrock_supervault_deposit: String,
+    pub maxbtc_supervault_deposit: String,
+    /// settlement account where funds will be sent to end users
+    pub settlement: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NeutronLibraries {
+    /// Deposit splitter where funds will be moved from deposit account to both Mars deposit or Supervault deposit according to the split ratio
+    pub deposit_splitter: String,
+    /// Mars lending library
+    pub mars_lending: String,
+    /// Supervault lpers
+    pub fbtc_supervault_lper: String,
+    pub lbtc_supervault_lper: String,
+    pub solvbtc_supervault_lper: String,
+    pub ebtc_supervault_lper: String,
+    pub pumpbtc_supervault_lper: String,
+    pub bedrock_supervault_lper: String,
+    pub maxbtc_supervault_lper: String,
+    /// Clearing queue
+    pub clearing_queue: String,
+    /// ICA transfer library
+    pub ica_transfer: String,
+    /// phase shift maxBTC issuer library
+    pub phase_shift_maxbtc_issuer: String,
+    /// phase shift splitter
+    pub phase_shift_splitter: String,
+    /// phase shift forwarder
+    pub phase_shift_forwarder: String,
+    /// phase shift supervault withdrawers
+    pub phase_shift_fbtc_supervault_withdrawer: String,
+    pub phase_shift_lbtc_supervault_withdrawer: String,
+    pub phase_shift_solvbtc_supervault_withdrawer: String,
+    pub phase_shift_ebtc_supervault_withdrawer: String,
+    pub phase_shift_pumpbtc_supervault_withdrawer: String,
+    pub phase_shift_bedrock_supervault_withdrawer: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NeutronCoprocessorAppIds {
+    /// Clearing queue
+    pub clearing_queue: String,
+}
