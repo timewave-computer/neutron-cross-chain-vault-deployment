d5b655e63a9c6568ca69fb26bd4195f443ea12d3
bekauz <bekauz@protonmail.com>
2025-06-27 15:13:47 +0200

    clean up settlement phase

--- strategies/lombard_btc/strategist/src/phases/settlement.rs
+++ strategies/lombard_btc/strategist/src/phases/settlement.rs
@@ -5,7 +5,7 @@ use log::{info, warn};
 use packages::{
     labels::{MARS_WITHDRAW_LABEL, SETTLE_OBLIGATION_LABEL},
     phases::SETTLEMENT_PHASE,
-    utils::{obligation::flatten_obligation_queue_amounts, valence_core},
+    utils::{obligation::batch_obligation_queue_payout_coins, valence_core},
 };
 use valence_clearing_queue_supervaults::msg::ObligationsResponse;
 use valence_domain_clients::cosmos::{base_client::BaseClient, wasm_client::WasmClient};
@@ -65,72 +65,69 @@ impl Strategy {
             target: SETTLEMENT_PHASE, "clearing queue length = {}", clearing_queue.obligations.len()
         );
 
-        // flatten the obligation response into amounts of relevant denoms
-        let (deposit_obligation_total, lp_obligation_total) = flatten_obligation_queue_amounts(
-            &clearing_queue.obligations,
-            (
-                self.cfg.neutron.denoms.deposit_token.to_string(),
-                self.cfg.neutron.denoms.supervault_lp.to_string(),
-            ),
-        );
+        // batch all active clearing queue obligations
+        let batched_obligation_coins =
+            batch_obligation_queue_payout_coins(&clearing_queue.obligations);
 
-        info!(
-            target: SETTLEMENT_PHASE, "total obligations deposit_token = {deposit_obligation_total}"
-        );
-        info!(
-            target: SETTLEMENT_PHASE, "total obligations supervaults_lp = {lp_obligation_total}"
-        );
+        for obligation_coin in batched_obligation_coins {
+            if obligation_coin.denom == self.cfg.neutron.denoms.deposit_token {
+                info!(target: SETTLEMENT_PHASE, "batched deposit_token obligation = {obligation_coin}");
+                // if there is insufficient amount of deposit token denom in the settlement account,
+                // we need to withdraw the delta from Mars lending position
+                if settlement_acc_bal_deposit < obligation_coin.amount.u128() {
+                    // simulate Mars protocol withdrawal to obtain the funds necessary
+                    // to fulfill all active withdrawal requests
+                    let obligations_delta = obligation_coin
+                        .amount
+                        .u128()
+                        .saturating_sub(settlement_acc_bal_deposit);
+                    info!(
+                        target: SETTLEMENT_PHASE, "settlement_account deposit_token balance deficit = {obligations_delta}"
+                    );
+                    info!(
+                        target: SETTLEMENT_PHASE, "withdrawing {obligations_delta} from mars lending position"
+                    );
 
-        // 3. if settlement account balance is insufficient to cover the active
-        // obligations, we perform the Mars protocol withdrawals
-        if settlement_acc_bal_deposit < deposit_obligation_total {
-            // 3. simulate Mars protocol withdrawal to obtain the funds necessary
-            // to fulfill all active withdrawal requests
-            let obligations_delta =
-                deposit_obligation_total.saturating_sub(settlement_acc_bal_deposit);
-            info!(
-                target: SETTLEMENT_PHASE, "settlement_account deposit_token balance deficit = {obligations_delta}"
-            );
+                    // call the Mars lending library to perform the withdrawal.
+                    // This will deposit the underlying assets directly to the settlement account.
+                    let mars_withdraw_msg =
+                        valence_library_utils::msg::ExecuteMsg::<_, ()>::ProcessFunction(
+                            valence_mars_lending::msg::FunctionMsgs::Withdraw {
+                                amount: Some(obligations_delta.into()),
+                            },
+                        );
 
-            // 4. call the Mars lending library to perform the withdrawal.
-            // This will deposit the underlying assets directly to the settlement account.
-            info!(
-                target: SETTLEMENT_PHASE, "withdrawing {obligations_delta} from mars lending position"
-            );
-            let mars_withdraw_msg =
-                valence_library_utils::msg::ExecuteMsg::<_, ()>::ProcessFunction(
-                    valence_mars_lending::msg::FunctionMsgs::Withdraw {
-                        amount: Some(obligations_delta.into()),
-                    },
-                );
+                    valence_core::enqueue_neutron(
+                        &self.neutron_client,
+                        &self.cfg.neutron.authorizations,
+                        MARS_WITHDRAW_LABEL,
+                        vec![to_json_binary(&mars_withdraw_msg)?],
+                    )
+                    .await?;
 
-            valence_core::enqueue_neutron(
-                &self.neutron_client,
-                &self.cfg.neutron.authorizations,
-                MARS_WITHDRAW_LABEL,
-                vec![to_json_binary(&mars_withdraw_msg)?],
-            )
-            .await?;
-
-            valence_core::tick_neutron(&self.neutron_client, &self.cfg.neutron.processor).await?;
-        }
-
-        if settlement_acc_bal_lp < lp_obligation_total {
-            warn!(target: SETTLEMENT_PHASE, "insufficient supervault LP share balance! available: {settlement_acc_bal_lp}, obligations: {lp_obligation_total}");
+                    valence_core::tick_neutron(&self.neutron_client, &self.cfg.neutron.processor)
+                        .await?;
+                }
+            } else if obligation_coin.denom == self.cfg.neutron.denoms.supervault_lp {
+                info!(target: SETTLEMENT_PHASE, "batched supervaults_lp obligation = {obligation_coin}");
+                if settlement_acc_bal_lp < obligation_coin.amount.u128() {
+                    warn!(target: SETTLEMENT_PHASE, "insufficient supervault LP share balance!
+                        available: {settlement_acc_bal_lp}, obligation: {obligation_coin}");
+                }
+            } else {
+                warn!(target: SETTLEMENT_PHASE, "unexpected coin among obligations: {obligation_coin}");
+            }
         }
 
         // 5. process the Clearing Queue settlement requests by enqueuing the settlement
         // messages to the processor and ticking
+        let settlement_exec_msg = valence_library_utils::msg::ExecuteMsg::<_, ()>::ProcessFunction(
+            valence_clearing_queue_supervaults::msg::FunctionMsgs::SettleNextObligation {},
+        );
         for obligation in clearing_queue.obligations {
             info!(
                 target: SETTLEMENT_PHASE, "settling obligation #{}", obligation.id
             );
-
-            // build the settlement function message
-            let settlement_exec_msg =
-                valence_library_utils::msg::ExecuteMsg::<_, ()>::ProcessFunction(
-                    valence_clearing_queue_supervaults::msg::FunctionMsgs::SettleNextObligation {},
-                );
 
             // enqueue the settlement message and tick the processor
             valence_core::enqueue_neutron(
