a27b8d91b1cb30ed46ec707e3e98f287c6b9d98f
Keyne <kleuyckx@gmail.com>
2025-07-29 11:32:18 +0100

    Redeploy verification gateway (#86)

--- packages/src/verification.rs
+++ packages/src/verification.rs
@@ -1,4 +1,4 @@
 pub const VALENCE_NEUTRON_VERIFICATION_GATEWAY: &str =
-    "neutron1s4s64kn07vvkp267aghsh9zy9wlwqsly9xvm858gsnzj20t3nk8qm5tn9p";
+    "neutron1l3fgzcqse0xw84hdpytg7vcp04kcdm95wes2zd6ap8kpujmv9cwsv45wwk";
 pub const VALENCE_ETHEREUM_VERIFICATION_GATEWAY: &str =
     "0xDe3f1195C54b74F9cAD78869f13C3A8C980abe02";
