04bb4cea2791a36d7120ea7124c1281713babeff
keyleu <kleuyckx@gmail.com>
2025-08-08 20:06:33 +0100

    update route

--- packages/src/verification.rs
+++ packages/src/verification.rs
@@ -1,5 +1,4 @@
 pub const VALENCE_NEUTRON_VERIFICATION_ROUTER: &str =
     "neutron1qef59cy20tf89mfhcj7mwnl22tq6ff9cmppqm4xm4d3u0s5hrsms4x5wlz";
 pub const VALENCE_ETHEREUM_VERIFICATION_ROUTER: &str = "0xb340bce9e0b06d572529582f3ab5b5633d38324b";
-// TODO: Update this when newest route is added
-pub const VERIFICATION_ROUTE: &str = "0000/sp1/5.0.8/groth16";
+pub const VERIFICATION_ROUTE: &str = "0001/sp1/5.0.8/groth16";
